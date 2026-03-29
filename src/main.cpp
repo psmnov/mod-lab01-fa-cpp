83d6b3340da573dbc9d7eeeaee5e2ecbddb006bb
psmnov <130275227+psmnov@users.noreply.github.com>
2026-03-29 22:26:28 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,10 @@
 #include "fun.h"
 
 int main() {
+    const char* myStr = "This 1s the3 senten$e With all caseS";
+    std::cout << myStr << "\n";
+    std::cout << "это 1я функция: " << faStr1(myStr) << "\n";
+    std::cout << "это 2я функция: " << faStr2(myStr) << "\n";
+    std::cout << "это 3я функция: " << faStr3(myStr) << "\n";
     return 0;
 }
