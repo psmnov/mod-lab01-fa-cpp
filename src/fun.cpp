b5ceecb8e0305616666960abfc4e59bc4052e284
psmnov <130275227+psmnov@users.noreply.github.com>
2026-03-29 21:12:31 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,25 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int count = 0;
+    bool inWord = false;
+    bool anyNumInWord = false;
+    while(*str) {
+        if (str!=' ') {
+            inWord = true;
+            if (*str >= '0' && *str <= '9') {
+                anyNumInWord = true;
+            }
+        }
+        else {
+            if (inWord && !anyNumInWord) {
+                count++;
+            }
+            inWord = false;
+            anyNumInWord = false;
+        }
+        str++;
+    }
 }
 
 unsigned int faStr2(const char *str) {
