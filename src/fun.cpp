e336e3fd1e68b47893cb44607401fcfd1140f869
psmnov <130275227+psmnov@users.noreply.github.com>
2026-03-29 22:17:11 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -64,6 +64,7 @@ unsigned int faStr3(const char *str) {
         str++;
     }
     if (count == 0) return 0;
-    int result = static_cast<int>(static_cast<double>(sumNumOfSymbol/count) + 0.5);
+    double preResult = static_cast<double>(sumNumOfSymbol/count)
+    int result = static_cast<int>(preResult + 0.5);
     return result;
 }
