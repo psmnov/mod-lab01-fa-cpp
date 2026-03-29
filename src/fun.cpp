71712b78e9f87bc14146b31343b0a10225a3f809
psmnov <130275227+psmnov@users.noreply.github.com>
2026-03-29 21:36:42 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -24,7 +24,29 @@ unsigned int faStr1(const char *str) {
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    int count = 0;
+    bool inWord = false;
+    bool anyIncorrectPatterns = false;
+    int numOfSymbol = 0;
+    while (*str) {
+        if (*str != ' ') {
+            inWord = true;
+            numOfSymbol++;
+            if ((numOfSymbol == 1 && (*str < 'A' || *str > 'Z'))
+                || (numOfSymbol != 1 && (*str < 'a' || *str > 'z'))) {
+                anyIncorrectPatterns = true;
+            }
+        } else {
+            if (inWord && !anyIncorrectPatterns) {
+                count++;
+            }
+            inWord = false;
+            anyIncorrectPatterns = false;
+            numOfSymbol = 0;
+        }
+        str++;
+    }
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
